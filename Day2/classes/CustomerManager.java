6392b993c8471b583734583b97fce85291b61ff9
mhmmtbilgin <muhammetbilqin@hotmail.com>
2021-04-28 00:19:30 +0300

    homework

---
+++ Day2/classes/CustomerManager.java
@@ -0,0 +1,18 @@
+package classes;
+
+public class CustomerManager {
+	public void Add() {
+		System.out.println("Musteri Eklendi.");
+	}
+	public void Remove() {
+		System.out.println("Musteri Silindi.");
+	}
+	public void Update() {
+		System.out.println("Musteri Guncellendi.");
+	}
+	
+	
+	
+}
+
+//Muhammet Bilgin...@mhmmtbilgin
---
+++ Day2/reCapDemo_Classes/DortIslem.java
@@ -0,0 +1,27 @@
+package reCapDemo_Classes;
+
+public class DortIslem {
+
+	public int topla(int sayi1, int sayi2) {
+		System.out.println("Toplamanin Sonucu: " + (sayi1+sayi2));
+		return sayi1 + sayi2;
+	}
+
+	public int cikar(int sayi1, int sayi2) {
+		System.out.println("Cikarmanin Sonucu : " + (sayi1-sayi2));
+		return sayi1 - sayi2;
+	}
+
+	public int carp(int sayi1, int sayi2) {
+		System.out.println("Carpmanin Sonucu : " + (sayi1*sayi2));
+		return sayi1 * sayi2;
+	}
+
+	public int bol(int sayi1, int sayi2) {
+		System.out.println("Bolmenin Sonucu : " + (sayi1/sayi2));
+		return sayi1 / sayi2;
+	}
+}
+
+
+//Muhammet Bilgin...@mhmmtbilgin
